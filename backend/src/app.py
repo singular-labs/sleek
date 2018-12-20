c876034735cce3516a345edbf0367d1b4947f849
Noa Hadar <noa.h@singular.net>
2018-12-20 12:27:57 +0200

    Sleek: added drawer - WIP

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask("Sleek")
 
+
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
--- frontend/src/components/Sleek.js
+++ frontend/src/components/Sleek.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 import AppBar from "@material-ui/core/es/AppBar/AppBar";
+import Drawer from "@material-ui/core/es/Drawer/Drawer";
+import Divider from '@material-ui/core/Divider';
 import Toolbar from "@material-ui/core/es/Toolbar/Toolbar";
-import IconButton from "@material-ui/core/es/IconButton/IconButton";
 import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from "@material-ui/core/es/IconButton/IconButton";
 import Typography from '@material-ui/core/Typography';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 
 class Sleek extends React.Component {
@@ -26,6 +29,19 @@ class Sleek extends React.Component {
                         </Typography>
                     </Toolbar>
                 </AppBar>
+                <Drawer
+                    variant="persistent"
+                    anchor="left"
+                    open={open}
+                >
+                    <div>
+                        <IconButton>
+                            <ChevronLeftIcon />
+                        </IconButton>
+                    </div>
+
+                    <Divider />
+                </Drawer>
             </div>
         );
     }
