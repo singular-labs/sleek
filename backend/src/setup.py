a128228c4e4b2639eacf3d8eb468aede36761f80
Itamar Hartstein <itamar@singular.net>
2019-01-07 22:21:07 -0800

    setup.py - some improvements - include requirements automatically and include the static directory when installing

--- backend/src/setup.py
+++ backend/src/setup.py
@@ -1,4 +1,14 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+REQUIREMENTS_FILE = os.path.abspath(os.path.join(__file__, "../requirements.txt"))
+
+
+def get_requirements(file_path):
+    return open(file_path).read().splitlines()
+
 
 setup(
     name="sleek",
@@ -6,5 +16,7 @@ setup(
     author="Singular",
     description="",
     packages=find_packages(),
-    # install_requires=[],        # TODO: Read from requirements
+    install_requires=get_requirements(REQUIREMENTS_FILE),
+    url="https://github.com/singular-labs/sleek",
+    include_package_data=True
 )
