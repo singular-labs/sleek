5eace464fdc44212c66df10fc2ec79379128d567
Itamar Hartstein <itamar@singular.net>
2019-02-12 22:26:49 -0800

    samples: fixes to timer script

--- backend/src/sleek/samples/timer.py
+++ backend/src/sleek/samples/timer.py
@@ -5,7 +5,13 @@ import time
 @click.command(help="Count down with the given number of seconds")
 @click.argument("seconds")
 def timer(seconds):
+    seconds = int(seconds)
+
     for i in range(seconds):
         print("%d seconds remaining" % (seconds - i))
         time.sleep(1)
     print("Done!")
+
+
+if __name__ == "__main__":
+    timer()
